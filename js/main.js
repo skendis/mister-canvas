5c5e0de6b53b6f7ef7e4a43b9b2ae7a2a3771026
skendis <sergei.kendis@gmail.com>
2019-06-05 15:54:01 +0300

    canvas-structure

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 'use strict'
 console.log('hello main.js');
 
+let canvas;
+let ctx;
 
-function onInit(){
-
+function onInit() {
     canvas = document.querySelector('#paintCanvas');
     ctx = canvas.getContext('2d')
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 }
+
